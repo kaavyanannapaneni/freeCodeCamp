aabbbab0e0e4892a9b878a41320efdb976de5f5e
Kaavya Nannapaneni <94700095+hello-im-kvy@users.noreply.github.com>
2022-03-03 16:38:05 +0530

    Create factorialize.js

---
+++ JavaScript Algorithms and Data Structures/Basic_Algorithm_Scripting/factorialize.js
@@ -0,0 +1,31 @@
+//using recursion
+
+function factorialize(num) {
+  if(num === 0){
+    return 1;
+  }
+  else if(num < 0){
+    return -1;
+  }
+  else
+  {
+  return num * factorialize(num-1);
+  }
+}
+
+console.log(factorialize(7));
+
+
+---
+
+function factorialize(num) {
+  let fact  = 1;
+  for(let i = num; i > 1 ; i--){
+      fact *= i; 
+  }
+  return fact;
+}
+
+console.log(factorialize(5));
+
+---
