e6daf122cc007ba73f813bcc257d6ad0ed673135
Kaavya Nannapaneni <94700095+hello-im-kvy@users.noreply.github.com>
2022-03-03 20:22:53 +0530

    Update Confirm the Ending.js

--- JavaScript Algorithms and Data Structures/Basic_Algorithm_Scripting/Confirm the Ending.js
+++ JavaScript Algorithms and Data Structures/Basic_Algorithm_Scripting/Confirm the Ending.js
@@ -1,10 +1,15 @@
+function confirmEnding(str, target) {
+console.log(str.length);
+console.log(target.length);
+let ending = str.slice(str.length - target.length) === target;
+return ending;
 
+}
 
-
-
-
+console.log(confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain"));
 
 ---
+  
 function confirmEnding(str, target) {
   if(str.endsWith(target)){
     return true;
